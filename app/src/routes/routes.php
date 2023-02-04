fe44f80bdfc55392be02c9dd2037c6d29760421c
Emilio Barbosa <emiliobarbos4@gmail.com>
2023-02-03 22:49:32 -0300

    fix login method on roues

--- app/src/routes/routes.php
+++ app/src/routes/routes.php
@@ -8,6 +8,6 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 $router = new \App\routes\Router();
 
 $router->get('/', [HomeController::class, 'index']);
-$router->get('/login', [HomeController::class, 'index']);
+$router->get('/login', [HomeController::class, 'login']);
 
 $router->resolve($_SERVER['REQUEST_URI'],strtolower($_SERVER['REQUEST_METHOD']));
